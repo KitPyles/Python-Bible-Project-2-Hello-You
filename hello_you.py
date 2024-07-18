8e5dab4393a525de369b3bc0c88e2829cabf99c0
Kit Pyles <pyles.kit@gmail.com>
2024-07-18 09:53:55 -0500

    Completed Project 2: Hello You!

---
+++ hello_you.py
@@ -0,0 +1,25 @@
+# Ask user for name
+
+name = input("What's your name? ")
+
+# Ask user for age
+
+age = input("How old are you? ")
+
+# Ask user for city
+
+city = input("What city do you live in? ")
+
+# Ask user what they enjoy
+
+enjoy = input("What do you do for fun? ")
+
+# Create output text
+
+# output = "Hello, {}. You are {} years old, live in {}, & enjoy {}.".format(name, age, city, enjoy)
+string = output = "Your name is {} and you are {} years old. You live in {}, and love {}.".format(name, age, city, enjoy)
+
+#Print output to screenKit
+
+# print(output)
+print(string)
